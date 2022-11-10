4e28dc66a6ebde96302c8d87d4ba080ae7d7f4c0
Sai Kovour <70875830+SuperSaigon123@users.noreply.github.com>
2022-11-10 12:33:21 -0600

    11/10 push 1
    I fixed some screens and began work on the functionality

--- app/src/main/java/com/example/fantasybasketballproject/SignInScreen.java
+++ app/src/main/java/com/example/fantasybasketballproject/SignInScreen.java
@@ -3,12 +3,19 @@ package com.example.fantasybasketballproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignInScreen extends AppCompatActivity {
+
+    EditText username = findViewById(R.id.username);
+    EditText password = findViewById(R.id.password);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
+
+
     }
+
 }
