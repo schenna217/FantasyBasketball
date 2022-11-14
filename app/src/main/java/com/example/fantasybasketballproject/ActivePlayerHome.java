1cf25de3fb707f9a7a54d8b131945a20a14207dd
schenna217 <90796839+schenna217@users.noreply.github.com>
2022-11-14 12:55:35 -0600

    Switched some screens code

--- app/src/main/java/com/example/fantasybasketballproject/ActivePlayerHome.java
+++ app/src/main/java/com/example/fantasybasketballproject/ActivePlayerHome.java
@@ -2,7 +2,11 @@ package com.example.fantasybasketballproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ActivePlayerHome extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class ActivePlayerHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_player_home);
+    }
+
+    public void switchScreens(View view) {
+        Button enterDraft = findViewById(R.id.enterDraft);
+        Intent intent = new Intent(this, ReadyToDraft.class);
+        startActivity(intent);
+    }
+
+    public void switchScreensRoster(View view) {
+        Button roster = findViewById(R.id.checkRoster);
+        Intent intent = new Intent(this, Roster.class);
+        startActivity(intent);
+    }
+
+    public void switchScreensBack(View view) {
+        Button logout = findViewById(R.id.logout);
+        Intent intent = new Intent(this, SignInScreen.class);
+        startActivity(intent);
     }
 }
