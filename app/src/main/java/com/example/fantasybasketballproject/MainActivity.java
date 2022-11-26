80b3b74021b9e39881ed9d98b907fe72e2842415
schenna217 <90796839+schenna217@users.noreply.github.com>
2022-11-26 02:07:14 -0600

    Worked on images code

--- app/src/main/java/com/example/fantasybasketballproject/MainActivity.java
+++ app/src/main/java/com/example/fantasybasketballproject/MainActivity.java
@@ -8,6 +8,12 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import kotlin.jvm.internal.CollectionToArray;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -29,4 +35,45 @@ public class MainActivity extends AppCompatActivity {
 //                .load("https://media.geeksforgeeks.org/wp-content/cdn-uploads/logo-new-2.svg")
 //                .into(imageView);
     }
+
+    String lebron = "123456";
+    String curry = "234567";
+    String giannis = "345678";
+
+
+    ArrayList<String> playerNumbers = new ArrayList<String>();
+
+    public String getLebron() {
+        return lebron;
+    }
+
+    public void setLebron(String lebron) {
+        this.lebron = lebron;
+    }
+
+    public String getCurry() {
+        return curry;
+    }
+
+    public void setCurry(String curry) {
+        this.curry = curry;
+    }
+
+    public String getGiannis() {
+        return giannis;
+    }
+
+    public void setGiannis(String giannis) {
+        this.giannis = giannis;
+    }
+
+    public ArrayList<String> getPlayerNumbers() {
+        return playerNumbers;
+    }
+
+    public void setPlayerNumbers(ArrayList<String> playerNumbers) {
+        this.playerNumbers = playerNumbers;
+    }
+
+
 }
