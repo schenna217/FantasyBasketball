f0a47818a75c0b7eb6dd1dabd0fd4b5de3286814
Eric Zhan <mightyerwa@gmail.com>
2022-11-28 12:55:19 -0600

    Working on Event Listener

--- app/src/main/java/com/example/fantasybasketballproject/FirstRound.java
+++ app/src/main/java/com/example/fantasybasketballproject/FirstRound.java
@@ -3,13 +3,16 @@ package com.example.fantasybasketballproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,12 +30,12 @@ public class FirstRound extends AppCompatActivity {
     private int Paul = 101108;
     private int Giannis = 203507;
 
-
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
-    int roomNum = 0;
     List<String> playerList =  Arrays.asList("Beal","Booker", "Murray");
     List<String> draftList;
+    int roomNum;
+    int turnNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,27 +45,52 @@ public class FirstRound extends AppCompatActivity {
         String P1 = "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/";
         String P2 = ".png";
         draftList = new ArrayList<>();
-
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("key");
+            //The key argument here must match that used in the other activity
+            roomNum = Integer.parseInt(value);
+        }
     }
-    public void arrangePlayers(){
 
+    ValueEventListener postListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // Get Post object and use the values to update the UI
+            Post post = dataSnapshot.getValue(Post.class);
+            // ..
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            // Getting Post failed, log a message
+            Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+        }
+    };
+mPostReference.addValueEventListener(postListener);
+    public void arrangePlayers(){
     }
     public void draftFunction(View view) {
+        int buttonNum = 0;
+        if (view.getId() == R.id.b1)
+        {
+            buttonNum = 0;
+            }
+        if (view.getId() == R.id.b2)
+        {
 
-        if (roomNum >= 0 && roomNum < playerList.size()) {
-            if (!playerList.get(roomNum).equals("Picked")) {
-                TextView player = (TextView) findViewById(R.id.textView);
-                draftList.add(playerList.get(roomNum));
-                System.out.println(draftList);
-                playerList.set(roomNum, "Picked");
-                System.out.println(playerList);
-                updateRoom(view);
+        }
+        if (!playerList.get(buttonNum).equals("Picked")) {
+            TextView player = (TextView) findViewById(R.id.textView);
+            draftList.add(playerList.get(roomNum));
+            System.out.println(draftList);
+            playerList.set(roomNum, "Picked");
+            System.out.println(playerList);
+            updateRoom(view);
             } else {
                 Toast.makeText(getApplicationContext(), "That player Has already been picked", Toast.LENGTH_LONG).show();
             }
-        } else {
-            Toast.makeText(getApplicationContext(), "Please Select a Value within the List", Toast.LENGTH_LONG).show();
-        }
+
     }
     public void updateRoom(View view)
     {
