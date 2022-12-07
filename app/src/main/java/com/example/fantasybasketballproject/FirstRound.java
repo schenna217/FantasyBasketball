7e26233cc88b14608bc6675003ac74ab29600c28
MiheerDGH <90772705+MiheerDGH@users.noreply.github.com>
2022-12-06 20:12:09 -0600

    worked on First Round

--- app/src/main/java/com/example/fantasybasketballproject/FirstRound.java
+++ app/src/main/java/com/example/fantasybasketballproject/FirstRound.java
@@ -1,29 +1,16 @@
 package com.example.fantasybasketballproject;
 
-import static java.util.Arrays.asList;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 
@@ -32,30 +19,38 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class FirstRound extends AppCompatActivity {
-    
-    ArrayList<String> playerssList = new ArrayList<String>(); // Create Playerlist
-    playerssList.add(Ball);
+public abstract class FirstRound extends AppCompatActivity {
+    private String Lamelo = "1630163";
+    private String Damian = "203081";
+    private String Kyrie = "202681";
+    private String Darius = "1629636";
+    private String Ja = "1629630";
+    private String Trae = "1629027";
+    private String Luka = "1629029";
+    private String Steph = "201939";
+    private String Chris = "101108";
+    private String Giannis = "203507";
+    private String Jalen = "1630224";
+    private String Klay = "202691";
+    private String Bradley = "203078";
+    private String Zach = "203897";
+    private String Anthony = "1630162";
+    private String Jaylen = "1627759";
+    private String Donovan = "1628378";
+    private String Devin = "1626164";
+
+
+    public List<String> distinctIDs = Arrays.asList(Lamelo, Damian, Kyrie, Darius, Ja, Trae, Luka, Steph, Chris
+            , Giannis, Jalen, Klay, Bradley, Zach, Anthony, Jaylen, Donovan, Devin);
+
+    ArrayList<Player> playerList = new ArrayList<Player>();
+    List<String> imageLinks = new ArrayList<String>(18);
+
+
+
     // add data below
-    playerssList
-    Player Ball = new Player(85, "Lamelo Ball", "point guard", "1630163", imageLinks.get(0));
-    Player Lillard = new Player(95, "Damian Lillard", "point guard", "203081");
-    Player Irving = new Player(90, "Kyrie Irving", "point guard", "202681");
-    Player Garland = new Player(85, "Darius Garland", "point guard", "1629636");
-    Player Morant = new Player(85, "Ja Morant", "point guard", "1630163");
-    Player Young = new Player(90, "Trae Young", "point guard", "1629027");
-    Player Doncic = new Player(95, "Luka Doncic", "point guard", "1629029");
-    Player Curry = new Player(90, "Steph Curry", "point guard", "201939");
-    Player Paul = new Player(85, "Chris Paul", "point guard", "101108");
-    Player Thompson = new Player(85, "Jalen Green", "shooting guard", "202691");
-    Player Green = new Player(85, "Jalen Green", "shooting guard", "1630224");
-    Player Akoumpo = new Player(95, "Giannis Antetokounmpo", "power forward", "203507");
-    Player Beal = new Player(90, "Bradley Beal", "shooting guard", "203078");
-    Player Lavine = new Player(90, "Zach Lavine", "shooting guard", "203897");
-    Player Edwards = new Player(85, "Anthony Edwards", "shooting guard", "1630162");
-    Player Brown = new Player(90, "Jaylen Brown", "shooting guard", "1627759");
-    Player Mitchell = new Player(90, "Donovan Mitchell", "shooting guard", "1628378");
-    Player Booker = new Player(90, "Devin Booker", "shooting guard", "1626164");
+
+
     // get method
 
     ImageButton imageButton;
@@ -63,9 +58,13 @@ public class FirstRound extends AppCompatActivity {
     DatabaseReference playerName;
     DatabaseReference playersRef;
 
-    List<String> imageLinks = new ArrayList<String>(18);
-    List<String> playerList = Arrays.asList(Mitchell, Ball, Lillard, Irving, Garland, Morant,
-            Young, Doncic, Curry, Paul, Giannis, Green, Thompson, Beal, Lavine, Edwards, Brown, Booker);
+    public ArrayList<Player> getPlayerList() {
+        return playerList;
+    }
+    public List<String> getDistinctIDs() {
+        return distinctIDs;
+    }
+
     List<String> draftList;
     int roomNum;
     int turnNum;
@@ -92,14 +91,29 @@ public class FirstRound extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_round);
-        arrangePlayers();
         draftList = new ArrayList<>();
-
-
+        createLinks();
+        Player Ball = new Player(85, "Lamelo Ball", "point guard", "1630163", imageLinks.get(0));
+        Player Lillard = new Player(95, "Damian Lillard", "point guard", "203081", imageLinks.get(1));
+        Player Irving = new Player(90, "Kyrie Irving", "point guard", "202681", imageLinks.get(2));
+        Player Garland = new Player(85, "Darius Garland", "point guard", "1629636", imageLinks.get(3));
+        Player Morant = new Player(85, "Ja Morant", "point guard", "1630163", imageLinks.get(4));
+        Player Young = new Player(90, "Trae Young", "point guard", "1629027", imageLinks.get(5));
+        Player Doncic = new Player(95, "Luka Doncic", "point guard", "1629029", imageLinks.get(6));
+        Player Curry = new Player(90, "Steph Curry", "point guard", "201939", imageLinks.get(7));
+        Player Paul = new Player(85, "Chris Paul", "point guard", "101108", imageLinks.get(8));
+        Player Thompson = new Player(85, "Jalen Green", "shooting guard", "202691", imageLinks.get(9));
+        Player Green = new Player(85, "Jalen Green", "shooting guard", "1630224", imageLinks.get(10));
+        Player Akoumpo = new Player(95, "Giannis Antetokounmpo", "power forward", "203507", imageLinks.get(11));
+        Player Beal = new Player(90, "Bradley Beal", "shooting guard", "203078", imageLinks.get(12));
+        Player Lavine = new Player(90, "Zach Lavine", "shooting guard", "203897", imageLinks.get(13));
+        Player Edwards = new Player(85, "Anthony Edwards", "shooting guard", "1630162", imageLinks.get(14));
+        Player Brown = new Player(90, "Jaylen Brown", "shooting guard", "1627759", imageLinks.get(15));
+        Player Mitchell = new Player(90, "Donovan Mitchell", "shooting guard", "1628378", imageLinks.get(16));
+        Player Booker = new Player(95, "Devin Booker", "shooting guard", "1626164", imageLinks.get(17));
+        arrangePlayers();
     }
-    public ArrayList<Player> getPlayerArrayList() {
-        return playerssList;
-    }
+
 
     public void onClick(View v) {
         switch (v.getId()) {
@@ -117,12 +131,10 @@ public class FirstRound extends AppCompatActivity {
     }
         public void arrangePlayers() {
 
-    public
 
 
 
-        List<String> playerList = Arrays.asList(Ball, Lillard, Irving, Garland, Morant, Young, Doncic,
-                Curry, Paul, Giannis, Green, Thompson, Beal, Lavine, Edwards, Brown);
+
 
             ImageView image1 = findViewById(R.id.imageView1);
             ImageView image2 = findViewById(R.id.imageView2);
@@ -143,47 +155,42 @@ public class FirstRound extends AppCompatActivity {
 
 
 
-           createLinks();
-
-            //Collections.shuffle(imageLinks);
+            Collections.shuffle(imageLinks);
 
 
             Log.i("KOVOUR", imageLinks.toString());
-
-            Picasso.get().load(imageLinks.get(1)).into(image1);
-            Picasso.get().load(imageLinks.get(2)).into(image2);
-            Picasso.get().load(imageLinks.get(3)).into(image3);
-            Picasso.get().load(imageLinks.get(4)).into(image4);
-            Picasso.get().load(imageLinks.get(5)).into(image5);
-            Picasso.get().load(imageLinks.get(6)).into(image6);
-            Picasso.get().load(imageLinks.get(7)).into(image7);
-            Picasso.get().load(imageLinks.get(8)).into(image8);
-            Picasso.get().load(imageLinks.get(9)).into(image9);
-            Picasso.get().load(imageLinks.get(10)).into(image10);
-            Picasso.get().load(imageLinks.get(11)).into(image11);
-            Picasso.get().load(imageLinks.get(12)).into(image12);
-            Picasso.get().load(imageLinks.get(13)).into(image13);
-            Picasso.get().load(imageLinks.get(14)).into(image14);
-            Log.i("KOVOUR", "loaded14");
-            Picasso.get().load(imageLinks.get(15)).into(image15);
-            Log.i("KOVOUR", "loaded15");
-            Picasso.get().load(imageLinks.get(16)).into(image16);
+            for(Player i : playerList) {
+                Picasso.get().load(i.imageLink).into(image1);
+                Picasso.get().load(imageLinks.get(2)).into(image2);
+                Picasso.get().load(imageLinks.get(3)).into(image3);
+                Picasso.get().load(imageLinks.get(4)).into(image4);
+                Picasso.get().load(imageLinks.get(5)).into(image5);
+                Picasso.get().load(imageLinks.get(6)).into(image6);
+                Picasso.get().load(imageLinks.get(7)).into(image7);
+                Picasso.get().load(imageLinks.get(8)).into(image8);
+                Picasso.get().load(imageLinks.get(9)).into(image9);
+                Picasso.get().load(imageLinks.get(10)).into(image10);
+                Picasso.get().load(imageLinks.get(11)).into(image11);
+                Picasso.get().load(imageLinks.get(12)).into(image12);
+                Picasso.get().load(imageLinks.get(13)).into(image13);
+                Picasso.get().load(imageLinks.get(14)).into(image14);
+                Picasso.get().load(imageLinks.get(15)).into(image15);
+                Picasso.get().load(imageLinks.get(16)).into(image16);
 
             }
-            public void createLinks(){
-                for (String i : playerList) {
-                    String P1 = "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/";
-                    String P2 = ".png";
+    public List<String> createLinks(){
+        for (String i : distinctIDs) {
+            String P1 = "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/";
+            String P2 = ".png";
 
-                    String createdLink = P1 + i + ".png";
+            String createdLink = P1 + i + ".png";
 
-                    imageLinks.add(createdLink);
-            }
-
-
-
-
+            imageLinks.add(createdLink);
+            return imageLinks;
+        }
     }
+
+
 
 
 
--- app/src/main/java/com/example/fantasybasketballproject/Player.java
+++ app/src/main/java/com/example/fantasybasketballproject/Player.java
@@ -5,9 +5,16 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Player implements Parcelable {
     private int rating;
@@ -35,39 +42,21 @@ public class Player implements Parcelable {
     private String Donovan = "1628378";
     private String Devin = "1626164";
 
-//    private ArrayList<String> playerList;
 
-//            =  Arrays.asList(Donovan, Lamelo, Damian, Kyrie, Darius, Ja, Steph, Trae, Luka
-//    , Chris, Giannis, Jalen, Klay, Bradley, Zach, Anthony, Jaylen, Devin);
-
-//    Player Ball = new Player(85, "Lamelo Ball", "point guard", "1630163");
-//    Player Lillard = new Player(95, "Damian Lillard", "point guard", "203081");
-//    Player Irving = new Player(90, "Kyrie Irving", "point guard", "202681");
-//    Player Garland = new Player(85, "Darius Garland", "point guard", "1629636");
-//    Player Morant = new Player(85, "Ja Morant", "point guard", "1630163");
-//    Player Young = new Player(90, "Trae Young", "point guard", "1629027");
-//    Player Doncic = new Player(95, "Luka Doncic", "point guard", "1629029");
-//    Player Curry = new Player(90, "Steph Curry", "point guard", "201939");
-//    Player Paul = new Player(85, "Chris Paul", "point guard", "101108");
-//    Player Thompson = new Player(85, "Jalen Green", "shooting guard", "202691");
-//    Player Green = new Player(85, "Jalen Green", "shooting guard", "1630224");
-//    Player Akoumpo = new Player(95, "Giannis Antetokounmpo", "power forward", "203507");
-//    Player Beal = new Player(90, "Bradley Beal", "shooting guard", "203078");
-//    Player Lavine = new Player(90, "Zach Lavine", "shooting guard", "203897");
-//    Player Edwards = new Player(85, "Anthony Edwards", "shooting guard", "1630162");
-//    Player Brown = new Player(90, "Jaylen Brown", "shooting guard", "1627759");
-//    Player Mitchell = new Player(90, "Donovan Mitchell", "shooting guard", "1628378");
-//    Player Booker = new Player(90, "Devin Booker", "shooting guard", "1626164");
+    public List<String> distinctIDs = Arrays.asList(Lamelo, Damian, Kyrie, Darius, Ja, Trae, Luka, Steph, Chris
+            , Giannis, Jalen, Klay, Bradley, Zach, Anthony, Jaylen, Donovan, Devin);
 
 
 
-    public Player(int rating, String name, String position, String imageID) {
-        this.rating = rating;
-        this.name = name;
-        this.position = position;
-        this.imageID = imageID;
-        this.imageViewID = "";
-    }
+
+
+
+
+
+
+
+
+
 
     public Player(int rating, String name, String position, String imageID, String imageLink) {
         this.rating = rating;
@@ -150,13 +139,10 @@ public class Player implements Parcelable {
     public void setposition(String position) {
         this.position = position;
     }
-
-
-
-
-    public List<String> getPlayerList(){
-        return playerList;
+    public List<String> getDistinctIDs() {
+        return distinctIDs;
     }
+
 
 }
 
