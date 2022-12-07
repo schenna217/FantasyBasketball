8cee0efc1cd9bb3dee1faa3d1d17169cb3e97624
MiheerDGH <90772705+MiheerDGH@users.noreply.github.com>
2022-12-06 23:42:29 -0600

    worked on creating new objects

--- app/src/main/java/com/example/fantasybasketballproject/SecondRound.java
+++ app/src/main/java/com/example/fantasybasketballproject/SecondRound.java
@@ -21,6 +21,14 @@ public class SecondRound extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_round);
 
+        Player Green = new Player(85, "Draymond Green", "power forward", "203110", "");
+        Player Siakam = new Player(90, "Pascal Siakam", "power forward", "1627783", "");
+        Player Banchero = new Player(85, "Paolo Banchero", "power forward", "1631094", "");
+        Player Davis = new Player(90, "Anthony Davis", "power forward", "203076", "");
+        Player Mobley = new Player(85, "Evan Mobley", "power forward", "1630596", "");
+        Player Towns = new Player(90, "Karl-Anthony Towns", "power forward", "1626157", "");
+        Player Williamson = new Player(90, "Zion Williamson", "power forward", "1629627", "");
+
 
 
 
